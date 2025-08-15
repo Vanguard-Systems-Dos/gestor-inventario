5f133c8e9cdd4bfef38b53324f5ab208dab01d73
AAMinoldo <minoldoaldo@gmail.com>
2025-08-15 19:09:56 -0300

    style: subo los estilo al app.css que es el estilo global

--- frontend/src/app/features/login/login.ts
+++ frontend/src/app/features/login/login.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
+  standalone: true,
   selector: 'app-login',
   imports: [],
   templateUrl: './login.html',
-  styleUrl: './login.css'
+  styleUrl: './login.css',
 })
 export class Login {
 
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,6 +4,7 @@ import { App } from './app/app';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
 
+
 bootstrapApplication(App, {
   providers: [provideRouter(routes),
   ]
