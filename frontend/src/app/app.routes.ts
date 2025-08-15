94e7c9e4eadb7bb44cd508839df306d3c1070fe4
AAMinoldo <minoldoaldo@gmail.com>
2025-08-15 11:22:20 -0300

    chore: configuracion de routes y carpetas

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { Home } from './features/home/home';
+import { Login } from './features/login/login';
+import { QuienesSomos } from './features/quienes-somos/quienes-somos';
+import { Dashboard } from './features/dashboard/dashboard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: Home, pathMatch: 'full' },
+    { path: 'login', component: Login },
+    { path: 'quienes-somos', component: QuienesSomos },
+    { path: 'dashboard', component: Dashboard },
+    { path: '**', redirectTo: '' }
+];
--- frontend/src/app/shared/navbar/navbar.ts
+++ frontend/src/app/shared/navbar/navbar.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { Header } from '../header/header';
 
 @Component({
+  standalone: true,
   selector: 'app-navbar',
-  imports: [],
+  imports: [RouterModule, Header],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css'
 })
