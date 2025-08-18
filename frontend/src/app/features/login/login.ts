6a370bb1b51f4b8bb5ac02208438b0eabe1086b0
AAMinoldo <minoldoaldo@gmail.com>
2025-08-18 18:30:03 -0300

    chore: funciones y estilos

--- frontend/src/app/features/login/login.ts
+++ frontend/src/app/features/login/login.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
+
 @Component({
   standalone: true,
   selector: 'app-login',
