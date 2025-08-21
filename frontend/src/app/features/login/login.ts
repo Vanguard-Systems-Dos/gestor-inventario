56726a3489115a04985832bbb5afb7ff7272a876
AAMinoldo <minoldoaldo@gmail.com>
2025-08-20 22:13:37 -0300

    refactor: agregamos set y style error en rojo

--- frontend/src/app/features/login/login.ts
+++ frontend/src/app/features/login/login.ts
@@ -19,18 +19,24 @@ export class Login implements OnInit {
     this.loginForm = this.fb.group(
       {
         email: ['', [Validators.required, Validators.email]],
-        password: ['', [Validators.required, Validators.minLength(6)]]
+        password: ['', [Validators.required]]
       }
     )
   }
 
   onSubmit(): void {
     if (this.loginForm.valid) {
-      console.log('Formulario valido:', this.loginForm.valid)
-      this.router.navigate(['/dashboard'])
+      console.log('Formulario valido:', this.loginForm.valid);
+      this.router.navigate(['/dashboard']);
     } else {
-      this.loginForm.markAllAsTouched()
+      this.loginForm.markAllAsTouched();
     }
   }
 
+  get email() {
+    return this.loginForm.get('email');
+  }
+  get password() {
+    return this.loginForm.get('password');
+  }
 }
