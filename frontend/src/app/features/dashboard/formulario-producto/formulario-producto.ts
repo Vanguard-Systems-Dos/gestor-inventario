563a58a120a7df4ec6df12a25d00d8d5ee048bbe
AAMinoldo <minoldoaldo@gmail.com>
2025-09-03 23:23:11 -0300

    feat: creacion de formulario-producto + activacion de botones en dashboard + creacion de producto.service

---
+++ frontend/src/app/features/dashboard/formulario-producto/formulario-producto.ts
@@ -0,0 +1,13 @@
+import { Component, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-formulario-producto',
+  imports: [CommonModule, FormsModule],
+  templateUrl: './formulario-producto.html',
+  styleUrl: './formulario-producto.css'
+})
+export class FormularioProducto {
+
+}
