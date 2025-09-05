813ae4be23d5d32a19b1a0e793b860582578826a
AAMinoldo <minoldoaldo@gmail.com>
2025-09-05 11:06:59 -0300

    feat: creacion de funciones resetProducto, cancelarFormulario, + style de formulario

--- frontend/src/app/features/dashboard/formulario-producto/formulario-producto.ts
+++ frontend/src/app/features/dashboard/formulario-producto/formulario-producto.ts
@@ -3,11 +3,15 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Iproducto } from '../../../models/iproducto';
 import { ProductoService } from '../../../services/producto.service';
+import { UnidadMedidaService } from '../../../services/unidad-medida.service';
+import { IUnidadMedida } from '../../../models/iunidad-medida';
+import { Observable } from 'rxjs';
+import { RouterModule } from '@angular/router';
 
 @Component({
   standalone: true,
   selector: 'app-formulario-producto',
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, RouterModule],
   templateUrl: './formulario-producto.html',
   styleUrls: ['./formulario-producto.css']
 })
@@ -26,19 +30,22 @@ export class FormularioProducto implements OnInit {
     stock_actual: 0
   }
 
+  unidadesMedidas$!: Observable<IUnidadMedida[]>
   mensaje = ''
 
-  constructor(private productoService: ProductoService) { }
+  constructor(private productoService: ProductoService,
+    private unidadMedidaService: UnidadMedidaService
+  ) { }
 
   ngOnInit(): void {
-
+    this.unidadesMedidas$ = this.unidadMedidaService.obtenerUnidadesMedidas()
   }
 
   altaProducto(producto: Iproducto) {
     this.productoService.crearProductos(this.producto).subscribe({
       next: (res => {
         this.mensaje = 'El producto se Guardo correctamente'
-
+        this.producto = this.resetProducto()
       }),
       error: (err => {
         console.error('Ocurrio un Error al Guardar Producto')
@@ -47,28 +54,25 @@ export class FormularioProducto implements OnInit {
     })
   }
 
+  cancelarFormulario(){
+    this.producto = this.resetProducto()
+  }  
 
-  unidadesMedida = [
-  { codigo: 'UN', nombre: 'Unidad' },
-  { codigo: 'CA', nombre: 'Caja' },
-  { codigo: 'PA', nombre: 'Paquete' },
-  { codigo: 'BOL', nombre: 'Bolsa' },
-  { codigo: 'BOT', nombre: 'Botella' },
-  { codigo: 'LAT', nombre: 'Lata' },
-  { codigo: 'ROL', nombre: 'Rollo' },
-  { codigo: 'M', nombre: 'Metro' },
-  { codigo: 'CM', nombre: 'Centímetro' },
-  { codigo: 'MM', nombre: 'Milímetro' },
-  { codigo: 'KG', nombre: 'Kilogramo' },
-  { codigo: 'G', nombre: 'Gramo' },
-  { codigo: 'L', nombre: 'Litro' },
-  { codigo: 'ML', nombre: 'Mililitro' },
-  { codigo: 'M2', nombre: 'Metro cuadrado' },
-  { codigo: 'M3', nombre: 'Metro cúbico' },
-  { codigo: 'PZ', nombre: 'Piezas' },
-  { codigo: 'JGO', nombre: 'Juego' },
-  { codigo: 'TAR', nombre: 'Tarro' }
-];
+  resetProducto(): Iproducto {
+    return {
+      nombre: '',
+      codigo_producto: '',
+      marca: '',
+      modelo: '',
+      unidad_medida: '',
+      descripcion: '',
+      fecha_creacion: undefined,
+      fecha_actualizacion: undefined,
+      estado: 'activo',
+      stock_actual: 0
+    }
+  }
+
 
 
 }
