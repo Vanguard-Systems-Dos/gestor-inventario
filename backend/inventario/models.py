2f90c8242eae98bed61dabdb454b53ecd2076982
nicolas minoldo <minoldonico@gmail.com>
2025-10-09 12:10:15 -0300

    build: creacion de clases categoria y proveedores en sus modelos correspondientes

--- backend/inventario/models.py
+++ backend/inventario/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Categoria(models.Model):
+    id_categoria = models.AutoField(primary_key=True)
+    nombre =  models.CharField(max_length=50,unique=True)
+    descripcion =  models.CharField(max_length=150, blank=True, null=True)
+
+    def __str__(self):
+        return self.nombre
--- backend/proveedores/models.py
+++ backend/proveedores/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Proveedor(models.Model):
+    id_proveedor = models.AutoField(primary_key=True)
+    razon_social =  models.CharField(max_length=100)
+    email =  models.EmailField(unique=True)
+    direccion  =  models.CharField(max_length=150)
+    telefono =  models.CharField(max_length=20,blank=True,null=True)
+
+    def __str__(self):
+        return self.razon_social
