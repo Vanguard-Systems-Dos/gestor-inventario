a11275d6958bb6bc952827b73d0e41a6a7f84e12
Maximiliano Scarpatti <91625757+MaxiScarpatti@users.noreply.github.com>
2025-10-11 15:18:19 -0300

    corrección simple de commit anterior

--- backend/movimientos/models.py
+++ backend/movimientos/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 
 # Create your models here.
-from django.db import models
-
 class UnidadMedida(models.Model):
     id_unidad_medida = models.AutoField(primary_key=True)
     codigo = models.CharField(max_length=10, unique=True)
