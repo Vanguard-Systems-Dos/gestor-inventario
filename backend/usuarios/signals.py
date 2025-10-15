b7e8ec74032c17f8cb8584baba11394157bea6f9
AAMinoldo <minoldoaldo@gmail.com>
2025-10-15 19:44:30 -0300

    fix: eliminar migracions viejas, ajustar el scripts signals, volver a migrar

--- backend/usuarios/signals.py
+++ backend/usuarios/signals.py
@@ -1,6 +1,7 @@
 from django.db.models.signals import post_migrate
 from django.dispatch import receiver
 from usuarios.models import Usuario
+from django.db import ProgrammingError, OperationalError
 
 usuarios_cargados = False  # variable global para controlar ejecución
 
@@ -38,17 +39,21 @@ def cargar_usuarios_post_migrate(sender, **kwargs):
         },
     ]
 
-    for data in usuarios_data:
-        usuario, created = Usuario.objects.get_or_create(
-            dni=data["dni"],
-            defaults={"nombre": data["nombre"], "rol": data["rol"]},
-        )
-        if created:
-            usuario.set_password(data["password"])
-            usuario.save()
-            print(f"✅ Usuario {usuario.nombre} creado automáticamente.")
-        else:
-            print(f"⚠️ Usuario {usuario.nombre} ya existe, se omitió.")
+    try:
+        for data in usuarios_data:
+            usuario, created = Usuario.objects.get_or_create(
+                dni=data["dni"],
+                defaults={"nombre": data["nombre"], "rol": data["rol"]},
+            )
+            if created:
+                usuario.set_password(data["password"])
+                usuario.save()
+                print(f"✅ Usuario {usuario.nombre} creado automáticamente.")
+            else:
+                print(f"⚠️ Usuario {usuario.nombre} ya existe, se omitió.")
 
-    usuarios_cargados = True
-    print("✅ Usuarios cargados.")
+        usuarios_cargados = True
+        print("✅ Usuarios cargados.")
+
+    except (ProgrammingError, OperationalError) as e:
+        print(f"⚠️ No se pudo cargar usuarios automáticamente: {e}")
